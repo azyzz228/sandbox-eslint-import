8c232f0a7f722a0423f58109046564a12ee91bfb
Aziz Abdullaev <aziz.saidmuratov@duke.edu>
2022-11-07 15:43:00 -0500

    initial commit

---
+++ debug/debug.js
@@ -0,0 +1,10 @@
+import bar, { multiply, sat } from "../src/foo.js"
+import * as b from "../src/foo"
+import * as d from "../src/foo"
+import * as e from "../src/foo"
+import * as f from "../src/foo"
+// if it runs above this 4 times. Make another map of specifier for the first one you found => content is there, just specifier name is different
+import * as a from "../src/bar"
+// continuation passing style 
+
+a.qwerty.third = 2;
